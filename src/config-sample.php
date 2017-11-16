a360bbe410221700a7debeeb327f695e3d44d170
Marci <marcacoyles@sky.com>
2017-11-16 08:56:15 +0000

    Add SSL setup...

--- src/config-sample.php
+++ src/config-sample.php
@@ -42,6 +42,80 @@ class Config {
     const DB_NAME       = '';
     const DB_USERNAME   = '';
     const DB_PASSWORD   = '';
+    
+    <?php
+/* ----------------------------------------------------------------------------
+ * Easy!Appointments - Open Source Web Scheduler
+ *
+ * @package     EasyAppointments
+ * @author      A.Tselegidis <alextselegidis@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2016, Alex Tselegidis
+ * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
+ * @link        http://easyappointments.org
+ * @since       v1.0.0
+ * ---------------------------------------------------------------------------- */
+
+/**
+ * Easy!Appointments Configuration File
+ *
+ * Set your installation BASE_URL * without the trailing slash * and the database
+ * credentials in order to connect to the database. You can enable the DEBUG_MODE
+ * while developing the application.
+ *
+ * Set the default language by changing the LANGUAGE constant. For a full list of
+ * available languages look at the /application/config/config.php file.
+ *
+ * IMPORTANT:
+ * If you are updating from version 1.0 you will have to create a new "config.php"
+ * file because the old "configuration.php" is not used anymore.
+ */
+class Config {
+
+    // ------------------------------------------------------------------------
+    // GENERAL SETTINGS
+    // ------------------------------------------------------------------------
+
+    const BASE_URL      = 'http://url-to-easyappointments-directory';
+    const LANGUAGE      = 'english';
+    const DEBUG_MODE    = FALSE;
+
+    // ------------------------------------------------------------------------
+    // DATABASE SETTINGS
+    // ------------------------------------------------------------------------
+
+    const DB_HOST       = '';
+    const DB_NAME       = '';
+    const DB_USERNAME   = '';
+    const DB_PASSWORD   = '';
+    
+    // ------------------------------------------------------------------------
+    // DATABASE SSL SETTINGS (mysqli only)
+    // see https://dev.mysql.com/doc/refman/5.7/en/mysql-ssl-rsa-setup.html
+    // for details on key generation
+    // ------------------------------------------------------------------------
+    
+    const DB_SSL        = FALSE;
+    const DB_SSLKEY     = '/etc/mysql/ssl/client-key.pem';
+    const DB_SSLCERT    = '/etc/mysql/ssl/client-cert.pem';
+    const DB_SSLCA      = '/etc/mysql/ssl/ca-cert.pem';
+    const DB_SSLCAPATH  = '/etc/mysql/ssl/';
+    const DB_SSLCIPHER  = NULL;
+    const DB_SSLVERIFY  = TRUE;
+
+    // ------------------------------------------------------------------------
+    // GOOGLE CALENDAR SYNC
+    // ------------------------------------------------------------------------
+
+    const GOOGLE_SYNC_FEATURE   = FALSE; // Enter TRUE or FALSE
+    const GOOGLE_PRODUCT_NAME   = '';
+    const GOOGLE_CLIENT_ID      = '';
+    const GOOGLE_CLIENT_SECRET  = '';
+    const GOOGLE_API_KEY        = '';
+}
+
+/* End of file config.php */
+/* Location: ./config.php */
+
 
     // ------------------------------------------------------------------------
     // GOOGLE CALENDAR SYNC
